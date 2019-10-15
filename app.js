f7f2a28cca60fe277bc0089913b7186b6b252cce
dangtrungdev <dangtrungdev113999@gmail.com>
2019-10-15 14:10:53 +0700

    create project

---
+++ app.js
@@ -0,0 +1,49 @@
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const app = express();
+
+const dbConnect = require("./config/dbConnect");
+
+const ProductRoute = require("./routes/product");
+const userRoute = require("./routes/user");
+
+// connect to mongo
+dbConnect();
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.use("/product", ProductRoute);
+app.use("/user", userRoute);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
