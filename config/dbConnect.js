7b735a90ab1a2bfd24ff93c2407a4f1234d49a70
dangtrungdev <dangtrungdev113999@gmail.com>
2019-10-22 09:03:59 +0700

    fix-db

--- config/dbConnect.js
+++ config/dbConnect.js
@@ -1,7 +1,8 @@
 const mongoose = require("mongoose");
 
 
-let URI = 'mongodb://trung-doan:trung-doan@ds137508.mlab.com:37508/heroku_gv549r6m';
+// let URI = 'mongodb://trung-doan:trung-doan@ds137508.mlab.com:37508/heroku_gv549r6m';
+let URI = 'mongodb://heroku_gv549r6m:7ahk2k3c3cgt93437ipuggdcf3@ds137508.mlab.com:37508/heroku_gv549r6m';
 
 let connectDB = async () => {
   try {
